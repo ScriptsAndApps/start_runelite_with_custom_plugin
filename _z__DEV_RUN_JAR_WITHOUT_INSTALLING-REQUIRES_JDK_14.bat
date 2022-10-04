2a2bdfa546c797d714df92ca8c69465d7ef3cd14
ScriptsAndApps <99158605+ScriptsAndApps@users.noreply.github.com>
2022-10-04 22:37:18 +0100

    Delete _z__DEV_RUN_JAR_WITHOUT_INSTALLING-REQUIRES_JDK_14.bat

--- _z__DEV_RUN_JAR_WITHOUT_INSTALLING-REQUIRES_JDK_14.bat
+++
@@ -1,3 +0,0 @@
-IF NOT EXIST C:\Users\%USERNAME%\AppData\Local\RuneLite\jre\bin\java.exe echo place jar in C:\Users\%USERNAME%\.runelite\ to run 
-C:\Users\%USERNAME%\AppData\Local\RuneLite\jre\bin\java.exe -Xms512m -jar -Duser.variant -ea C:\Users\%USERNAME%\.runelite\RuneLite.jar
-pause
